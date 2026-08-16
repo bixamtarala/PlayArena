40581365fd9be3fbc9797ac3953ac408f0f8f386
bixamtarala <bixam.tarala@gmail.com>
2026-08-16 16:43:14 +0530

    Add PlayArena mobile smoke test

---
+++ mobile/test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:playarena/main.dart';
+
+void main() {
+  testWidgets('PlayArena development preview loads', (tester) async {
+    await tester.pumpWidget(const PlayArenaApp());
+    expect(find.text('PlayArena'), findsOneWidget);
+    expect(find.text('Cricket + Teen Patti'), findsOneWidget);
+    expect(find.text('Open Preview'), findsOneWidget);
+  });
+}
